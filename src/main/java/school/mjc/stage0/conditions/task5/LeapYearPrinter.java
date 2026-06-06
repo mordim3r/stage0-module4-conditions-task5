32548a0cca6596f9366c062667472e31b501fd2f
mordim3r <valent1n.bazylev@gmail.com>
2026-06-06 14:12:06 +0300

    add

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("Leap year");
+        } else {
+            System.out.println("Not leap year");
+        }
     }
 }
